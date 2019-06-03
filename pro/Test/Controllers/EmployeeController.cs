ebb1ba52a6888e9e4e875f8a88f689b9ec594208
kai <kai255201@yahoo.com.tw>
2019-06-03 19:18:21 +0800

    co

--- pro/Test/Controllers/EmployeeController.cs
+++ pro/Test/Controllers/EmployeeController.cs
@@ -13,7 +13,6 @@ namespace Test.Controllers
 {
     public class EmployeeController : Controller
     {
-        NorthwindEntities db = new NorthwindEntities();
         private IRepository<Employees> Employee = new Repository<Employees>();
         // GET: /Employee/
         public ActionResult Index()
@@ -24,7 +23,7 @@ namespace Test.Controllers
         public ActionResult Search()
         {
             JQueryDataTableRequest jqDataTableRq = JQueryDataTableHelper<Employees>.GetRequest(Request);
-            var q = db.Employees.AsEnumerable();
+            var q = Employee.GetAll();
             int totalLen = Convert.ToInt16(q.Count());
              //排序
             q = q.OrderBy(jqDataTableRq.orderBy +"  "+ jqDataTableRq.orderDir).ToList();
@@ -42,5 +41,81 @@ namespace Test.Controllers
             return Json(jqDataTableRs);
         }
 
+        #region Create
+        [HttpPost]  
+        public ActionResult Create(string LastName, string FirstName,  string HomePhone)
+        {
+            try
+            {
+                Employees emp = new Employees();
+                emp.LastName = LastName;
+                emp.FirstName = FirstName;
+                emp.HomePhone = HomePhone;
+                Employee.Create(emp);
+                return Json(new { Status = "0", StatusDesc = "新增成功!!" });
+            }
+            catch (Exception err)
+            {
+                 
+                return Json(new { Status = "2", StatusDesc = "新增失敗" + err.Message });
+            }           
+        }
+        #endregion
+
+        #region Delete
+        [HttpPost]
+        public ActionResult Delete(string Id)
+        {
+            try
+            {
+                Employees _Employees = Employee.GetById(int.Parse(Id));
+                Employee.Delete(_Employees);
+                return Json(new { Status = "0", StatusDesc = "刪除成功" });
+            }
+            catch (Exception err)
+            {
+
+                return Json(new { Status = "2", StatusDesc = "刪除失敗" + err.Message });
+            }
+
+        }
+        #endregion
+
+        #region Get
+        [HttpPost]
+        public ActionResult Get(string Id)
+        {
+            try
+            {
+                Employees _Employees = Employee.GetById(int.Parse(Id));
+                return Json(new { Status = "0", StatusDesc = _Employees });
+            }
+            catch (Exception err)
+            {
+
+                return Json(new { Status = "2", StatusDesc = "失敗" + err.Message });
+            }
+        }
+        #endregion
+
+        [HttpPost]
+        public ActionResult Edit(string Id,string LastName, string FirstName, string HomePhone)
+        {
+            try
+            {
+                Employees _Employees = Employee.GetById(int.Parse(Id));
+                _Employees.LastName = LastName;
+                _Employees.FirstName = FirstName;
+                _Employees.HomePhone = HomePhone;
+                Employee.Update(_Employees);
+                return Json(new { Status = "0", StatusDesc ="修改成功" });
+            }
+            catch (Exception err)
+            {
+
+                return Json(new { Status = "2", StatusDesc = "修改失敗" + err.Message });
+            }
+ 
+        }
 	}
 }
